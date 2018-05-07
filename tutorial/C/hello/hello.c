990a389b11c14f52b681741de9fd34e2d74ac513
casualflavor <26201078+casualflavor@users.noreply.github.com>
2018-05-08 00:07:04 +0800

    New C tutorial: hello

---
+++ tutorial/C/hello/hello.c
@@ -0,0 +1,12 @@
+// Include the header file of standard input/output library.
+#include <stdio.h>
+
+// Main function is the entry of C program.
+int main(int argc, char **argv)
+{
+	// Print the "Hello World!" string to screen, and "\n" is newline symbol.
+	printf("Hello World!\n");
+
+	// 0 as the return value of this program, means the program is terminted normally.
+	return 0;
+}
