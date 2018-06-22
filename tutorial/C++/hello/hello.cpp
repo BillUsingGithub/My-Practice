806a00a0b2ee99da4b549a5cb24868179414b327
casualflavor <26201078+casualflavor@users.noreply.github.com>
2018-06-23 04:30:41 +0800

    Add hello of C++

---
+++ tutorial/C++/hello/hello.cpp
@@ -0,0 +1,12 @@
+// Include the input/output stream library.
+#include <iostream>
+
+// Main function is the entry of C++ program.
+int main(int argc, char *argv[])
+{
+	// Print the "Hello World!" string to screen, then end the line.
+	std::cout << "Hello World!" << std::endl;
+
+	// 0 as the return value of this program, means the program is terminted normally.
+	return 0;
+}
